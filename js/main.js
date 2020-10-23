cf80c692906117bc752628e2a4f5dcf0209f6997
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-23 09:26:47 +0200

    Fix problems with main sections and start coding javascript

--- js/main.js
+++ js/main.js
@@ -4,4 +4,17 @@ console.log("Hola mundo");
 
 const buttonSearch = document.querySelector(".js-btnSearch");
 
-/* buttonSearch.addEventListener("click", searchShow); */
+function getDataFromApi() {
+  const inputSearch = document.querySelector(".js-input");
+  const inputValue = inputSearch.value;
+  console.log(inputValue);
+  fetch("http://api.tvmaze.com/search/shows?q=")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      /* for(const show of data.show) */
+    });
+}
+
+buttonSearch.addEventListener("click", getDataFromApi);

c1e3021e27ccfffc73c272eb994572c145b32f74
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-23 10:04:23 +0200

    Add fetch and function getData from API

--- js/main.js
+++ js/main.js
@@ -2,19 +2,29 @@
 
 console.log("Hola mundo");
 
-const buttonSearch = document.querySelector(".js-btnSearch");
+let series = [];
+
+//Función para obtener los datos del API
 
 function getDataFromApi() {
   const inputSearch = document.querySelector(".js-input");
   const inputValue = inputSearch.value;
   console.log(inputValue);
-  fetch("http://api.tvmaze.com/search/shows?q=")
+  fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      /* for(const show of data.show) */
+      for (const serie of data) {
+        series.push(serie.show);
+        console.log(series);
+      }
     });
 }
 
+const buttonSearch = document.querySelector(".js-btnSearch");
 buttonSearch.addEventListener("click", getDataFromApi);
+
+//borrar antes de subir
+
+buttonSearch.click();

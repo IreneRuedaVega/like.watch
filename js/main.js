2c1af64b3c0ed1a101a5d0a6b36e150e72c21105
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-23 11:18:21 +0200

    Add function paintSeries and listenBotton

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 "use strict";
 
-console.log("Hola mundo");
+const resultsShow = document.querySelector(".js-list");
 
 let series = [];
 
@@ -9,22 +9,39 @@ let series = [];
 function getDataFromApi() {
   const inputSearch = document.querySelector(".js-input");
   const inputValue = inputSearch.value;
-  console.log(inputValue);
   fetch(`http://api.tvmaze.com/search/shows?q=${inputValue}`)
     .then(function (response) {
       return response.json();
     })
     .then(function (data) {
-      for (const serie of data) {
-        series.push(serie.show);
-        console.log(series);
-      }
+      series = data;
+      paintSeries();
+      listenButton();
     });
 }
 
+//Función para pintar
+
+function paintSeries() {
+  let seriesResults = "";
+  let urlphoto = "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+  for (let i = 0; i < series.length; i++) {
+    seriesResults += `<li class="card">`;
+    seriesResults += `<h2 class="card__title">${series[i].show.name}</h2>`;
+    if (series[i].show.image === null) {
+      seriesResults += `<img class="card__img" src="${urlphoto}" alt="Foto de ${series[i].show.name}"/>`;
+    } else {
+      seriesResults += `<img class="card__img" src="${series[i].show.image.medium}" alt="Foto de ${series[i].show.name}"/>`;
+    }
+    seriesResults += "<li>";
+  }
+  resultsShow.innerHTML = seriesResults;
+}
+
 const buttonSearch = document.querySelector(".js-btnSearch");
-buttonSearch.addEventListener("click", getDataFromApi);
 
-//borrar antes de subir
+function listenButton() {
+  buttonSearch.addEventListener("click", getDataFromApi);
+}
 
-buttonSearch.click();
+listenButton();

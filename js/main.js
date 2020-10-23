e59a214209c900865dd6d6ba4f50d83fc417cad6
IreneRuedaVega <ruedavega.irene@gmail.com>
2020-10-23 17:02:40 +0200

    Add function paintFavourites and function favouritesShows

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const resultsShow = document.querySelector(".js-list");
+const resultsFavouritesShows = document.querySelector(".js-list-favourites");
 
 let series = [];
 let favouritesList = [];
@@ -53,24 +54,64 @@ function paintSeries() {
 
 //Función para las series favoritas
 
+//const favouritesShows = function (event) {
+//console.log("Hola mundo");
+//const clicked = parseInt(event.currentTarget.id);
+//console.log(clicked);
+//const indexFav = favouritesList.indexOf(clicked); // find
+//const isFavourite = indexFav !== -1;
+//if (isFavourite === false) {
+//favouritesList.push(clicked); // guardar el objeto entero
+//} else {
+//favouritesList.splice(indexFav, 1);
+//}
+//console.log(favouritesList);
+//paintSeries();
+//listenSeries();
+//paintFavourites();
+//};
+
 const favouritesShows = function (event) {
   console.log("Hola mundo");
-  const clicked = parseInt(event.currentTarget.id);
-  console.log(clicked);
-  const indexFav = favouritesList.indexOf(clicked); // find
-  const isFavourite = indexFav !== -1;
-  if (isFavourite === false) {
-    favouritesList.push(clicked); // guardar el objeto entero
-  } else {
-    favouritesList.splice(indexFav, 1);
+  const clickedSerie = parseInt(event.currentTarget.id);
+  let serieFav;
+  let indexInFavourites;
+  for (let i = 0; i < series.length; i++) {
+    console.log("entro");
+    if (clickedSerie === series[i].show.id) {
+      serieFav = favouritesList.find(
+        (favouritesList) => favouritesList.show.id == series[i].id
+      );
+      if (serieFav !== undefined) {
+        indexInFavourites = favouritesList.findIndex((fav) => serieFav === fav);
+        favouritesList.splice([indexInFavourites], 1);
+      } else {
+        favouritesList.push(series[i]);
+      }
+    }
   }
-  console.log(favouritesList);
+
   paintSeries();
   listenSeries();
-  //paintFavourites();
+  paintFavourites();
 };
 
-/* function paintFavourites(){} */
+function paintFavourites() {
+  let seriesFavouriteResults = "";
+  let urlPhotoFavourites =
+    "https://via.placeholder.com/210x295/ffffff/666666/?text=TV";
+  for (let i = 0; i < favouritesList.length; i++) {
+    seriesFavouriteResults += `<li class="card" id="${favouritesList[i].show.id}">`;
+    seriesFavouriteResults += `<h2 class="card__title">${favouritesList[i].show.name}</h2>`;
+    if (favouritesList[i].show.image === null) {
+      seriesFavouriteResults += `<img class="card__img" src="${urlPhotoFavourites}" alt="Foto de ${favouritesList[i].show.name}"/>`;
+    } else {
+      seriesFavouriteResults += `<img class="card__img" src="${favouritesList[i].show.image.medium}" alt="Foto de ${favouritesList[i].show.name}"/>/>`;
+    }
+    seriesFavouriteResults += "<li>";
+  }
+  resultsFavouritesShows.innerHTML = seriesFavouriteResults;
+}
 
 function listenSeries() {
   const seriesItems = document.querySelectorAll(".card");
